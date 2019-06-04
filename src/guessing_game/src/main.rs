18b2321fbbc94a22081e5c226d3af8a7be267a2e
gsvolt <gsvolt7@gmail.com>
2019-06-04 11:57:48 -0400

    refactored guessing game

--- src/guessing_game/src/main.rs
+++ src/guessing_game/src/main.rs
@@ -1,14 +1,39 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng; // trait
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();  // mutable
+    println!("The secret number is: {}", secret_number);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    loop{
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();  // mutable
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){ 
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
+    let age: i8 = 16;
+    println!("{}", age);
 }
